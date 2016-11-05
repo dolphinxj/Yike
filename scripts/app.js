7f1a12a0044bb37b3205a2b5bfbda310ae71ea60
xujing <xujing@163.com>
2016-11-06 01:19:50 +0800

    完成侧边栏

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,22 @@
-angular.module('Yike', ['$ngRoute']).
-config(['$routeProvider',function($routeProvider) {
-		$routeProvider.when('/', {
-			templateUrl:'../views/body.html'
-		})
-	}])
+var Yike = angular.module('Yike', ['ngRoute']);
+Yike.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: './views/body.html',
+        
+    })
+    .when('/older',{
+    	templateUrl:'./views/older.html',
+       
+    })
+}]);
+
+
+Yike.run(['$rootScope',function($rootScope){
+	$rootScope.collapsed = false;
+	$rootScope.toggle = function(){
+		$rootScope.collapsed = !$rootScope.collapsed;
+	}
+}])
+
+
+
