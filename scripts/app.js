d96352f87e6f53e55b566c9a8c5f414fd4fbcbae
xujing <xujing@163.com>
2016-11-06 16:47:40 +0800

    完成了侧栏特效

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,55 @@
-var Yike = angular.module('Yike', ['ngRoute']);
+var Yike = angular.module('Yike', ['ngRoute', 'Controllers']);
 Yike.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
+    $routeProvider.when('/today', {
         templateUrl: './views/body.html',
-        
+        controller: 'TodayController'
     })
     .when('/older',{
     	templateUrl:'./views/older.html',
-       
+        controller: 'OlderController'
+    })
+    .otherwise({
+        redirectTo: '/today'
     })
 }]);
 
-
+// 处理侧栏特效
 Yike.run(['$rootScope',function($rootScope){
 	$rootScope.collapsed = false;
 	$rootScope.toggle = function(){
 		$rootScope.collapsed = !$rootScope.collapsed;
+
+        var navs = document.querySelectorAll('.navs dd');
+        if($rootScope.collapsed) {
+            // 显示
+
+            angular.forEach(navs, function (val, key) {
+
+                val.style.transitionDuration = 0.3 * key + 's';
+
+                val.style.transitionDelay = '0.3s';
+
+                val.style.transform = 'translate(0)';
+
+            });
+
+        } else {
+            // 隐藏
+
+            var len = navs.length - 1; // 5;
+            angular.forEach(navs, function (val, key) {
+
+                // len - key;
+                navs[len - key].style.transitionDuration = key * 0.3 + 's';
+                navs[len - key].style.transform = 'translate(-100%)';
+                navs[len - key].style.transitionDelay = '0';
+
+            });
+        }
+    
 	}
+
+
 }])
 
 
